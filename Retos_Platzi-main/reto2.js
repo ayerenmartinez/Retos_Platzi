f10292553167025cc177a27aebd1600c79deec38
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2022-08-20 16:45:54 -0500

    Reto 2 finalizado

---
+++ Retos_Platzi-main/reto2.js
@@ -0,0 +1,9 @@
+/*Reto #2 “Hola… nombre y apellido:”
+Instrucciones: Ahora que sabemos incluir nombres,
+ podemos agregar más datos. Intentemos con un apellido para 
+tener algo así: ``Hola, [nombre] [apellido]*/
+let nombre;
+let apellidos;
+nombre = prompt("Ingrese sus Nombre: ");
+apellidos =prompt("Ingrese sus Apellidos: ");
+console.log("Hola, "+nombre+" "+apellidos)
